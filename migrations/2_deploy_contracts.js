f3c76e125e59629b3b77386d0cf3f4a9b9c27322
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-09-21 21:22:19 +0200

    Initialize TokenFarm contract

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const DappToken = artifacts.require('DappToken');
+const DaiToken = artifacts.require('DaiToken');
+const TokenFarm = artifacts.require('TokenFarm');
+
+module.exports = async function(deployer, network, accounts) {
+  // deploy the moc DAI token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  // deploy the Dapp token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  // deploy the TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // transfer all tokens to the TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+  // transfer 100 moc DAI tokens to an investor
+  await daiToken.transfer(accounts[1], '100000000000000000000');
+};
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
   string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
+
+  // runs once when the smart contract is deployed to the network
+  constructor(DappToken _dappToken, DaiToken _daiToken) public {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+  }
 }
