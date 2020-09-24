0e9ec083ee8c31fe3171eb49c6c673da441c0a51
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-09-24 22:15:50 +0200

    Implement stakeTokens function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,9 +8,36 @@ contract TokenFarm {
   DappToken public dappToken;
   DaiToken public daiToken;
 
+  address[] public stakers;
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaking;
+
   // runs once when the smart contract is deployed to the network
   constructor(DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
   }
+
+  // staking tokens (deposit)
+  function stakeTokens(uint _amount) public {
+    // transfer mock DAI tokens to this contract for staking
+    daiToken.transferFrom(msg.sender, address(this), _amount);
+
+    // update staking balance
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+    // add user to stakers array only if they haven't staked already
+    if (!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+    }
+
+    // update staking status
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+  }
+
+  // unstaking tokens (withdraw)
+
+  // issuing tokens
 }
