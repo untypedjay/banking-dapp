216e48a5bb6e208990b978da4253871386e8d086
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-09-26 19:51:13 +0200

    Implement issueTokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,6 +5,7 @@ import "./DaiToken.sol";
 
 contract TokenFarm {
   string public name = "Dapp Token Farm";
+  address public owner;
   DappToken public dappToken;
   DaiToken public daiToken;
 
@@ -17,10 +18,14 @@ contract TokenFarm {
   constructor(DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
+    owner = msg.sender;
   }
 
   // staking tokens (deposit)
   function stakeTokens(uint _amount) public {
+    // require amount greater than 0
+    require(_amount > 0, "amount cannot be 0");
+    
     // transfer mock DAI tokens to this contract for staking
     daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -40,4 +45,17 @@ contract TokenFarm {
   // unstaking tokens (withdraw)
 
   // issuing tokens
+  function issueTokens() public {
+    // only owner can call this function
+    require(msg.sender == owner, "caller must be the owner");
+
+    // issue tokens to all stakers
+    for (uint i = 0; i < stakers.length; i++) {
+      address recipient = stakers[i];
+      uint balance = stakingBalance[recipient];
+      if (balance > 0) {
+        dappToken.transfer(recipient, balance);
+      }
+    }
+  }
 }
